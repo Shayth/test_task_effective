31abcf802c4fdbdb80a48363374b285a414cc69e
Shayth <Shaythyce@gmail.com>
2023-08-20 16:02:53 +0300

    grooming

--- main.py
+++ main.py
@@ -48,13 +48,12 @@ def load_from_file():
 def main():
     # Основная функция main
     data = load_from_file()
-
     page_size: int = 5
     number_of_lines = len(data)
     amount_of_pages: int = -(-number_of_lines // page_size)
 
     while True:
-        print("\nТелефонный справочник")
+        print("\nТелефонный справочник\n")
         print("1. Вывод записей")
         print("2. Добавление записи")
         print("3. Редактирование записи")
@@ -63,25 +62,30 @@ def main():
         print("6. Выход")
         choice = input("Выберите действие: ")
 
-        # Выбор пункта меню
+        # Вывод записей по страницам
         if choice == "1":
             print(f'В данный момент в справочнике {number_of_lines} записей')
             print(f'Количество доступных страниц: {amount_of_pages}')
             selected_page = int(input('Введите необходимую страницу: '))
             display_page(page_size, selected_page, data)
+        # Добавление записи в справочник
         elif choice == "2":
             new_entry = input("Введите новую запись: ")
             add_entry(data, new_entry)
+        # Редактирование записи в справочнике
         elif choice == "3":
             index = int(input("Введите индекс записи для редактирования: ")) - 1
             new_entry = input("Введите новую запись: ")
             edit_entry(data, index, new_entry)
+        # Поиск записей в справочнике
         elif choice == "4":
             user_search_input = input("Введите значения через запятую для поиска: ").split(',')
             search_entries(data, user_search_input)
+        # Вывод текста помощи
         elif choice == "5":
             print('Это телефонный справочник.\nВсе данные в нем хранятся в следующем виде:'
                   '\nФамилия, имя, отчество, название организации, телефон рабочий, телефон личный.')
+        # Стоп
         elif choice == '6':
             print('Работа программы остановлена.')
             break
